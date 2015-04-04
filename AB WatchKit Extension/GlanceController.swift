7efeb33caf039ab0e6753754757511a5d6604f58
Cameron Bernhardt <cambernhardt@me.com>
2015-04-03 23:18:55 -0400

    Added WatchKit target

---
+++ AB WatchKit Extension/GlanceController.swift
@@ -0,0 +1,31 @@
+//
+//  GlanceController.swift
+//  AB WatchKit Extension
+//
+//  Created by Cameron Bernhardt on 4/3/15.
+//  Copyright (c) 2015 Cameron Bernhardt. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class GlanceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
